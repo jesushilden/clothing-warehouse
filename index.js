958accc7051e09cd858e0cf632992fdd8b3f5808
Peter Hilden <peter.e.hilden@gmail.com>
2021-03-07 17:22:24 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+process.env.NODE_ENV === 'development' && require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
